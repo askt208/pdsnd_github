f95c589200f30102da1dc94418ccb7c83ecdad6a
PeiShan Wu <askt208@hotmail.com>
2022-11-29 22:02:08 +0800

    merge with refactoring

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 >**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
 
 ### Date created
-Include the date you created this project and README file.
+Created this project and README file on 29th, November, 2022.
 
 ### Project Title
-Replace the Project Title
+Investigate a Relational Database
 
 ### Description
-Describe what your project is about and what it does
+You can explore the bike share usage pattern in chicago, Washington, and New York City, from Januaryin to June in 2017.
 
 ### Files used
-Include the files used
+bikeshare_2.py
+README.md
+.gitignore
 
-### Credits
-It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
-
+### Credit
+Udacity
--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! So happy to see you. Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     valid_cities = ['washington','chicago','new york city']
     city = input("Choose one of the three cities, chicago, new york city, or washington: ").lower()
@@ -173,8 +173,8 @@ def main():
         trip_duration_stats(df)
         view_5_rows(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart or say goodbye? Enter restart or goodbye.\n')
+        if restart.lower() != 'restart':
             break
 
 
